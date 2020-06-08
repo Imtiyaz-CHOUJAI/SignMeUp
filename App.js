e031700e6eeedf35f54d2e6fd9b3455ec16fac6b
Imtiyaz CHOUJAI <choujai.imtiyaz@gmail.com>
2020-06-08 23:11:37 +0100

    YEEET :tada:

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, StatusBar, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <Text>Hello world</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
